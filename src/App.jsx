c10cfe1d709dfd82be894870605e134c98c98bf1
Ahmad-Mukhtar-Bhatti <24100079@lums.edu.pk>
2023-09-12 22:11:29 +0500

    src folder

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Homepage from './components/homepage/Homepage';
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/homepage/Homepage.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+import './homepage.css'
+
+const Homepage = () => {
+  return (
+    <>
+        
+    <h1>hello</h1>
+
+    </>
+  )
+}
+
+
+export default Homepage
