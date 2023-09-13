858e9f8770b0679e0c926f41d4aee54cb908d15c
Muhammad Jafri <114304752+Muhammad-Jafri@users.noreply.github.com>
2023-09-13 22:53:21 +0500

    tweaking with usestate

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,11 +1,20 @@
 // import React from 'react'
+import { useState } from 'react'
 import './homepage.css'
 
 const Homepage = () => {
+
+  let i = 0
+  const [marks, setMarks] = useState(0)
+  
   return (
+    
     <>
         
-    <h1>hello</h1>
+    <h1>my marks are {marks}</h1>
+    <button onClick={() => {
+      setMarks(marks + 1)
+    }}>change</button>
 
     </>
   )
